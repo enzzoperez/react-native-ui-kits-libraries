cedb80929e596de6585de9a451b0ebd4e7b85163
Enzo Perez <enzzorp@gmail.com>
2021-06-19 03:18:05 -0300

    Update thisWillHelpYou.js

--- thisWillHelpYou.js
+++ thisWillHelpYou.js
@@ -1,6 +1,5 @@
 //Button stars and forked generator
-//https://michaelcurrin.github.io/badge-generator/#/repo
+[Tool to generate forks and stars buttons](https://michaelcurrin.github.io/badge-generator/#/repo)
 
-
-//NPM downloads button generator (search npm in list)
-//https://shields.io/category/downloads
+//NPM downloads button generator (search npm in list)                   
+[Tool to generate npm downloads buttons](https://shields.io/category/downloads)
