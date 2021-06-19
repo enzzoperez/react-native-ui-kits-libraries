f4370f805748929d6d06de76c8c012c6301b5e5a
Enzo Perez <enzzorp@gmail.com>
2021-06-19 02:00:11 -0300

    Create thisWillHelpYou.js

---
+++ thisWillHelpYou.js
@@ -0,0 +1,6 @@
+//Button stars ad forked generator
+//https://michaelcurrin.github.io/badge-generator/#/repo
+
+
+//NPM downloads button generator (search npm in list)
+//https://shields.io/category/downloads
